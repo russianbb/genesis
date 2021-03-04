5dcce25d816f3e06b2b13a89f80aefe6656d3024
Bruno Borges Russian <bruno.russian.i@gmail.com>
2021-03-03 23:51:34 -0300

    Hotfix requirements

--- src/genesis/core/urls.py
+++ src/genesis/core/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings  # TODO: remover no deploy
-from django.conf.urls.static import static  # TODO: remover no deploy
 from django.contrib import admin
 from django.urls import include, path
 
@@ -13,6 +11,4 @@ urlpatterns = [
     path(r"ping/", ping),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
-] + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)  # TODO: remover no deploy
+]
