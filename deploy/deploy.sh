2f1b0475a10af51716e110543d694d9769d754c4
Bruno Borges Russian <bruno.russian.i@gmail.com>
2021-06-28 21:47:03 -0300

    Adiciona scripts de deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,19 +1,21 @@
-echo "--> Cleaning Docker"
+echo $(date ++%x-%r)  "#### Stasting deploy.sh" >> mylog.txt
+
+echo $(date ++%x-%r)  "--> Cleaning Docker" >> mylog.txt
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker image rm $(docker image ls -aq)
 
-echo "--> Login docker to ECR"
+echo $(date ++%x-%r)  "--> Login docker to ECR" >> mylog.txt
 aws ecr get-login-password --region sa-east-1 | docker login --username AWS --password-stdin $ECR_DNS
 
-echo "--> Pulling image $TAG"
-docker pull $ECR_DNS:$TAG
+echo $(date ++%x-%r)  "--> Pulling image" >> mylog.txt
+docker pull $ECR_DNS:latest
 
-echo "--> Running Django migrations on Docker."
-docker run --env-file .env --rm $ECR_DNS:$TAG python manage.py migrate
+echo $(date ++%x-%r)  "--> Running Django migrations on Docker." >> mylog.txt
+docker run --env-file .env --rm $ECR_DNS:latest python manage.py migrate
 
-echo "--> Running Django collectstatic on Docker."
-docker run --env-file .env --rm $ECR_DNS:$TAG python manage.py collectstatic
+# echo $(date ++%x-%r)  "--> Running Django collectstatic on Docker." >> mylog.txt
+# docker run --env-file .env --rm $ECR_DNS:latest python manage.py collectstatic
 
-echo "--> Running Django Server."
-docker run --env-file .env $ECR_DNS:$TAG
+echo $(date ++%x-%r)  "--> Running Django Server." >> mylog.txt
+docker run --env-file .env $ECR_DNS:latest
