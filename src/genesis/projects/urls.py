8880083cf8b78087a1ed1b89d4c83d09f1a41d13
Bruno Borges Russian <bruno.russian.i@gmail.com>
2021-08-10 23:28:40 -0300

    Add ProjectDetailView

--- src/genesis/projects/urls.py
+++ src/genesis/projects/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ProjectListView
+from .views import ProjectDetailView, ProjectListView
 
 app_name = "projects"
 
 urlpatterns = [
     path("projetos/", ProjectListView.as_view(), name="list"),
+    path("projetos/<int:pk>", ProjectDetailView.as_view(), name="detail"),
 ]
--- src/genesis/projects/views.py
+++ src/genesis/projects/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from .models import Project
 
@@ -8,3 +8,12 @@ class ProjectListView(LoginRequiredMixin, ListView):
     template_name = "projects/list.html"
     context_object_name = "projects"
     model = Project
+
+
+class ProjectDetailView(LoginRequiredMixin, DetailView):
+    template_name = "projects/detail.html"
+    model = Project
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.prefetch_related("companies")
