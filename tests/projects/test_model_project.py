30149d1c2a521ba36f6ee39d17aaf320663aa9d1
Bruno Borges Russian <bruno.russian.i@gmail.com>
2021-08-29 01:15:15 -0300

    Add email methods do Project

--- tests/projects/test_model_project.py
+++ tests/projects/test_model_project.py
@@ -11,6 +11,10 @@ def test_project_date_display(project):
     assert project.date_display == "31/12/2020"
 
 
+def test_project_date_display_dash(project):
+    assert project.date_display_dash == "31-12-2020"
+
+
 def test_project_status_display(project):
     assert project.status_display == "Ativo"
 
@@ -18,3 +22,11 @@ def test_project_status_display(project):
 def test_project_company_str(project_company):
     expected_str = "31/12/2020 - Circularização de Estoque - Republica Jabuti LTDA ME"
     assert str(project_company) == expected_str
+
+
+def test_project_initial_template(project):
+    assert project.initial_email_template == "email/circularizacao/initial.html"
+
+
+def test_project_email_from(project):
+    assert project.email_from == "Onix Soluções Empresariais <projetos@onixse.com>"
