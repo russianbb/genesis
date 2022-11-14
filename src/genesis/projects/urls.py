8b2486a8b7d222f11844ae24031560adafe7e300
russianbb <bruno.russian.i@gmail.com>
2022-11-14 00:39:58 -0500

    Add project documents list view

--- src/genesis/projects/urls.py
+++ src/genesis/projects/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import ProjectDetailView, ProjectListView
+from .views import ProjectCompanyDocumentList, ProjectDetailView, ProjectListView
 
 app_name = "projects"
 
 urlpatterns = [
     path("projetos/", ProjectListView.as_view(), name="list"),
     path("projetos/<int:pk>", ProjectDetailView.as_view(), name="detail"),
+    path(
+        "projetos/documentos/", ProjectCompanyDocumentList.as_view(), name="documents"
+    ),
 ]
